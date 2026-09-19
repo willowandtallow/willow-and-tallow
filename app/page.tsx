3392368bd628de99c33552c88f6869fddc5e1538
willowandtallow <291915009+willowandtallow@users.noreply.github.com>
2026-09-19 14:20:00 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import Reveal from "@/components/Reveal";
+import Reveal from "./components/Reveal";
 
 /* =========================================================
    PRODUCTS
